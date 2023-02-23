e2d915cf98aa156c5659a061d612db75e5359e11
Anirudh Boddu <anirudh.boddu97@gmail.com>
2023-02-23 15:01:47 -0500

    Added extra field and moved Main class to controllers folder

--- RealEstateManagementSystem/src/main/java/controllers/RealEstateManagementSystem.java
+++ RealEstateManagementSystem/src/main/java/controllers/RealEstateManagementSystem.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.controllers;
 
 public class RealEstateManagementSystem {
     
--- RealEstateManagementSystem/src/main/java/models/Lease.java
+++ RealEstateManagementSystem/src/main/java/models/Lease.java
@@ -8,6 +8,7 @@ public class Lease {
 	   private double rentAmount;
 	   private Date startDate;
 	   private Date endDate;
+	   private Boolean isActive;
 
 	   public Lease(Tenant tenant, Property property, double rentAmount, Date startDate, Date endDate) {
 	       this.tenant = tenant;
@@ -15,6 +16,7 @@ public class Lease {
 	       this.rentAmount = rentAmount;
 	       this.startDate = startDate;
 	       this.endDate = endDate;
+	       this.isActive =  false;
 	   }
 
 	   public Tenant getTenant() {
@@ -48,5 +50,9 @@ public class Lease {
 	   public void setEndDate(Date endDate) {
 	       this.endDate = endDate;
 	   }
+
+	   public Boolean getActive() { return isActive; }
+
+	   public void setActive(Boolean active) { isActive = active; }
 	}
 
