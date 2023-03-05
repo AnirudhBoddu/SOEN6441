8d3bab6c3487a5b5fd08f3938c3d6edfb467e3b1
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-03-05 00:08:07 -0500

    fix phoneNumber type

--- RealEstateManagementSystem/src/test/java/models/TenantTest.java
+++ RealEstateManagementSystem/src/test/java/models/TenantTest.java
@@ -10,11 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TenantTest {
 
     private Tenant tenant;
-    private Database dbInstance = Database.getInstance();
+    private final Database dbInstance = Database.getInstance();
 
     @BeforeEach
     public void setUp() {
-        tenant = new Tenant("John", "Doe", "johndoe@example.com", 5555555);
+        tenant = new Tenant("John", "Doe", "johndoe@example.com", "5555555");
         dbInstance.getTenants().clear();
     }
 
@@ -28,9 +28,9 @@ public class TenantTest {
         tenant.setLastName("Smith");
         assertEquals("Smith", tenant.getLastName());
 
-        assertEquals(5555555, tenant.getPhoneNumber());
-        tenant.setPhoneNumber(1234567);
-        assertEquals(1234567, tenant.getPhoneNumber());
+        assertEquals("5555555", tenant.getPhoneNumber());
+        tenant.setPhoneNumber("1234567");
+        assertEquals("1234567", tenant.getPhoneNumber());
 
         assertEquals("johndoe@example.com", tenant.getEmail());
         tenant.setEmail("janesmith@example.com");
