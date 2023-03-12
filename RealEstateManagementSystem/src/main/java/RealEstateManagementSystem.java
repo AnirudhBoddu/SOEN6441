35f8b9289ba7d4bfabafadf143dab26c0c7e4a4d
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-03-12 12:03:19 -0400

    remove redundant code

--- RealEstateManagementSystem/src/main/java/RealEstateManagementSystem.java
+++ RealEstateManagementSystem/src/main/java/RealEstateManagementSystem.java
@@ -16,25 +16,6 @@ public class RealEstateManagementSystem {
     public static void main(String[] args) throws ParseException, FileNotFoundException {
         System.out.println("Real Estate Management System");
         displayMenu();
-//        PropertyFactory propertyFactory = new HouseFactory();
-//        Address address = new Address(321, "Oak St", "Anytown", "Anystate", "67891");
-//        PropertySpecification propertySpec = new PropertySpecification(1, 1, 1000);
-//        Property p = propertyFactory.createProperty(null, address, propertySpec, null, 1050);
-//        Database dbInstance = Database.getInstance();
-//        dbInstance.addProperty(p);
-//
-//        Tenant tenant;
-//        String firstName = "John";
-//        String lastName = "Doe";
-//        String phoneNumber = "1234567890";
-//        String email = "johndoe@example.com";
-//        tenant = new Tenant(firstName, lastName, email, phoneNumber);
-//        dbInstance.addTenant(tenant);
-//
-//
-//        LeaseController.getInstance().displayVacantUnits();
-
-
     }
 
     private static void displayMenu() {
